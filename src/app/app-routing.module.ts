0698214bfe2f0e7e130e1de99122dd389cbaf810
Kaushal Pohekar <kaushalpohekar1@gmail.com>
2023-06-27 15:03:10 +0530

    Login Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,8 @@
-/*import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './login/auth/auth.guard';
-import { RoleGuard } from './services/role.guard';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'dash', loadChildren: () => import('./dashboard/dashboard.module' ).then(m => m.DashboardModule), canActivate: [AuthGuard, RoleGuard], data:{roles: ['Standard', 'Admin']} },
-  { path: 'sa', loadChildren: () => import('./super-admin/super-admin.module').then(m => m.SuperAdminModule), canActivate: [AuthGuard, RoleGuard], data:{roles: ['Super Admin']} },
-  { path: '**', redirectTo: 'login'}
-  // Add other routes if needed
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-*/
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './login/auth/auth.guard';
 import { RoleGuard } from './services/role.guard';
+import { LoginGuard } from './login/auth/login.guard';
 
 import { LoginLayoutComponent } from './login/login-layout/login-layout.component';
 import { DashLayoutComponent } from './dashboard/dash-layout/dash-layout.component';
@@ -32,6 +13,7 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginLayoutComponent,
+    canActivate: [LoginGuard],
     children: [
       { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
     ]
